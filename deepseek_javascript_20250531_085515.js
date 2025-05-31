a6e82f6850bf8d9fa028cde7029c875d416d9ef8
2fizll2 <2fizll2@mail.ru>
2025-05-31 18:33:19 +0500

    Add files via upload

---
+++ deepseek_javascript_20250531_085515.js
@@ -0,0 +1,9 @@
+// Замените этот блок в index.html
+const firebaseConfig = {
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_ПРОЕКТ.firebaseapp.com",
+  projectId: "ВАШ_ПРОЕКТ",
+  storageBucket: "ВАШ_ПРОЕКТ.appspot.com",
+  messagingSenderId: "ВАШ_SENDER_ID",
+  appId: "ВАШ_APP_ID"
+};
